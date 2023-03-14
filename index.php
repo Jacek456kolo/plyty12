4ebc6d276f1efc652b019190dd6ef9bc2abcc266
Jacek Gach <projekt@ceti.com.pl>
2023-03-14 14:29:52 +0100

    first

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		
+		
+		
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>PHP - Hello, World!</title>
+		
+		<link href="stylesheet1.css" rel="stylesheet">
+					
+	</head>
+	
+	
+	
+	<body>
+		
+		
+		<div id="tlo1">
+			
+			<div id="przesun1">
+				<div id="tlo3">
+					<div id="pc1">1PC</div>
+					<div id="pc2">-----------------</div>
+					<div id="pc3">1ROK</div>
+				</div>
+			</div>
+			
+			<div id="naglowek1">G DATA</div>
+			<div id="naglowek2">Software OEM</div>
+		</div>
+		<div id="naglowek3">Zabezpieczenie komputera przed wirusami, ransomeware i innymi zagrozeniami z sieci</div>
+		<div id="kolumny1">
+			<div> <img src="ransomware1.jpg" width="100"; height="100;">
+			</div>
+			<div> <img src="logo2.jpg">
+			</div>
+		</div>
+		<div id="tlo2">
+			<div id="kolumny2">
+				<div> <img src="pcicon.png" width="20"; height="20;">
+				</div>
+				<div id="background1">2018
+				</div>
+			</div>
+		</div>
+	</body>
+</html> 			
